65334387d39093c3f534a5c25ec13df03ea3cb43
Andrew Katson <katsonandrew3.5@gmail.com>
2025-02-13 15:21:25 -0800

    Update assembly.pl

--- assembly.pl
+++ assembly.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl
 
 # Open the output file in write mode
-open(my $fh, '>', 'assembly.asm') or die "Cannot open file: $!";
+open(my $fh, '>', 'assembly.s') or die "Cannot open file: $!";
 
 # Write a single line of assembly code to the file
 print $fh "mov rax, 1   ; This is a simple Linux x86_64 assembly instruction\n";
@@ -9,4 +9,4 @@ print $fh "mov rax, 1   ; This is a simple Linux x86_64 assembly instruction\n";
 # Close the file
 close($fh);
 
-print "Assembly code has been written to output.asm\n";
+print "Assembly code has been written to assembly.s\n";
